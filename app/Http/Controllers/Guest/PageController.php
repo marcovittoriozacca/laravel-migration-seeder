e7b9c0c6be1f3fb678c827cacbd9ca3a8765ff7d
marcovittoriozacca <zaccamarco01@gmail.com>
2024-03-15 15:52:53 +0100

    feat: train with departure time from the current date on

--- app/Http/Controllers/Guest/PageController.php
+++ app/Http/Controllers/Guest/PageController.php
@@ -10,11 +10,15 @@ use Illuminate\Support\Facades\Schema;
 class PageController extends Controller
 {
     public function index(){
-
+        //risultato della query
         $trains = Train::all();
 
+        //i treni che partono dalla data odierna in poi
+        $today_trains = Train::where('departure_time', '>=', today())->get();
+
+        //colonne del db
         $columns =  Schema::getColumnListing('trains');
 
-        return view('home', compact('trains', 'columns'));
+        return view('home', compact('trains', 'columns', 'today_trains'));
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -33,4 +33,35 @@
   </table>
 
 
+  <h1>Treni in partenza da oggi in poi</h1>
+
+
+<table class="table table-striped">
+    <thead class="text-center">
+        <tr>
+            @foreach ($columns as $col)
+            @if ($loop->remaining >= 2 )
+            <th scope="col">
+                {{$col}}
+            </th>
+            @endif
+            @endforeach
+        </tr>
+    </thead>
+    <tbody class="text-center">
+        @foreach ($today_trains as $train)            
+        <tr>
+            <th scope="row">{{$train['id']}}</th>
+            @foreach ($train->toArray() as $train_info)
+            @if (!$loop->first && $loop->remaining >= 2)    
+            <td>
+            {{$train_info}}
+            </td>
+            @endif
+            @endforeach
+        </tr>
+        @endforeach
+    </tbody>
+</table>
+
 @endsection
